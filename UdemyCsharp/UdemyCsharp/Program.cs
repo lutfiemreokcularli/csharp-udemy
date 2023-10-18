f360d51887cd38384e280b067430c3fa54e06208
unknown <lutfiemreokcularli@gmail.com>
2023-10-18 10:03:05 +0300

    csharp first commit

---
+++ UdemyCsharp/UdemyCsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UdemyCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Age = 22;
+            ogr.Name = "Name";
+            ogr.Surname = "Surname";
+
+            Console.WriteLine(ogr.Name);
+            Console.WriteLine(ogr.Surname);
+            Console.WriteLine(ogr.Age);
+        }
+    }
+    class Ogrenci
+    {
+        private string _name;
+        private string _surname;
+        private int _age;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Surname { get { return _surname; } set { _surname = value; } }
+        public int Age { get { return _age; } set { 
+            
+                if(value > 30)
+                {
+                    throw new System.Exception("Öğrenci yaşı 30dan büyük olamaz");
+                }
+                _age = value;
+            } }
+    }
+}
